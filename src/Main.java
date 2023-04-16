05d2d5635e755490c092897444cbfcaa91ed7488
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2023-04-16 21:13:59 +0200

    created mySparseMatrix "main" class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        double[] row1 = {1,0};
-        double[] row2 = {2,0};
-        double[][] matrix = new double[2][2];
-        matrix = new double[][]{row1, row2};
-        Ds2 matrixDs2 = new Ds2(matrix);
-        Ds3 matrixDs3 = new Ds3(matrix);
-        matrixDs2.printNewMatrix();
-        matrixDs3.printNewMatrix();
 
     }
 }
---
+++ src/MySparseMatrix/MySparseMatrix.java
@@ -0,0 +1,28 @@
+package MySparseMatrix;
+
+import java.util.ArrayList;
+
+// "MySparseMatrix" represents a matrix over the field of real numbers.
+public class MySparseMatrix {
+    private ArrayList<ArrayList<Number>> sparsified_matrix;
+
+    private void setSparsified_matrix(ArrayList<ArrayList<Number>> sparsified_matrix) {
+        this.sparsified_matrix = sparsified_matrix;
+    }
+
+    /**
+     * DS2 arrayList in which each element describes one row of a matrix,
+     * where a matrix row can be another list or a dictionary.
+     */
+    //
+
+    public void sparsify_ds2(ArrayList<ArrayList<Number>> matrix){
+        // TODO
+    }
+
+    public void sparsify_ds3(ArrayList<ArrayList<Number>> matrix){
+        // TODO
+    }
+
+
+}
