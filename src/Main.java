729e4aea1164ebff57c392d9a2ad0dcc15b8132a
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2023-04-16 22:58:39 +0200

    created implementation of ds3 with another variable in class //finished class

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,44 @@
 import MySparseMatrix.MySparseMatrix;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<ArrayList<Double>> matrix = new ArrayList<>() {{
+        ArrayList<ArrayList<Double>> matrix1 = new ArrayList<>() {{
             add(new ArrayList<>() {{ add(1.0); add(1.0); add(0.0); }});
             add(new ArrayList<>() {{ add(2.0); add(0.0); add(0.0); }});
             add(new ArrayList<>() {{ add(0.0); add(3.0); add(3.0); }});
         }};
 
-        MySparseMatrix matrix_ds2 = new MySparseMatrix();
+        ArrayList<HashMap<Integer, Double>> matrix2 = new ArrayList<>() {{
+            add(new HashMap<>(){{
+                put(0, 1.0);
+                put(1, 1.0);
+                put(2, 0.0);
+            }});
+            add(new HashMap<>(){{
+                put(0, 2.0);
+                put(1, 0.0);
+                put(2, 0.0);
+            }});
+            add(new HashMap<>(){{
+                put(0, 0.0);
+                put(1, 3.0);
+                put(2, 3.0);
+            }});
 
-        matrix_ds2.sparsify_ds2(matrix);
-        System.out.println(matrix_ds2.get_sparsified_matrix());
+        }};
+
+
+        MySparseMatrix matrix_machine = new MySparseMatrix();
+
+        matrix_machine.sparsify_ds2(matrix1);
+        System.out.println(matrix_machine.get_sparsified_matrix_ds2());
+
+        matrix_machine.sparsify_ds3(matrix2);
+        System.out.println(matrix_machine.get_sparsified_matrix_ds3());
 
 
 
--- src/MySparseMatrix/MySparseMatrix.java
+++ src/MySparseMatrix/MySparseMatrix.java
@@ -1,35 +1,50 @@
 package MySparseMatrix;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 // "MySparseMatrix" represents a matrix over the field of real numbers.
 public class MySparseMatrix {
     public MySparseMatrix(){
         System.out.println("matrix machine has been set up");
     }
-    private ArrayList<ArrayList<Double>> sparsified_matrix;
+    private ArrayList<ArrayList<Double>> sparsified_matrix_ds2;
+    private ArrayList<HashMap<Integer, Double>> sparsified_matrix_ds3;
 
-    private void set_sparsified_matrix(ArrayList<ArrayList<Double>> sparsified_matrix) {
-        this.sparsified_matrix = sparsified_matrix;
-    }
 
-    public ArrayList<ArrayList<Double>> get_sparsified_matrix() {
-        return sparsified_matrix;
+    public ArrayList<ArrayList<Double>> get_sparsified_matrix_ds2() {
+        return sparsified_matrix_ds2;
     }
 
     /**
      * DS2 arrayList in which each element describes one row of a matrix,
-     * where a matrix row can be another list or a dictionary.
+     * where a matrix row is another Arraylist.
      */
-    //
 
     public void sparsify_ds2(ArrayList<ArrayList<Double>> matrix){
         matrix.forEach(row -> row.removeIf(element -> element == 0.0));
         set_sparsified_matrix(matrix);
     }
 
-    public void sparsify_ds3(ArrayList<ArrayList<Number>> matrix){
-        // TODO
+    /**
+     * DS3 arrayList in which each element describes one column of a matrix,
+     * where a matrix column is a HashMap (dictionary).
+     */
+
+    public void sparsify_ds3(ArrayList<HashMap<Integer, Double>> matrix){
+        matrix.forEach(row -> row.entrySet().removeIf(entry -> entry.getValue() == 0.0));
+        set_sparsified_matrix_ds3(matrix);
     }
 
+    private void set_sparsified_matrix_ds3(ArrayList<HashMap<Integer, Double>> sparsified_matrix) {
+        this.sparsified_matrix_ds3 = sparsified_matrix;
+    }
+
+    private void set_sparsified_matrix(ArrayList<ArrayList<Double>> sparsified_matrix) {
+        this.sparsified_matrix_ds2 = sparsified_matrix;
+    }
+
+    public ArrayList<HashMap<Integer, Double>> get_sparsified_matrix_ds3() {
+        return sparsified_matrix_ds3;
+    }
 }
